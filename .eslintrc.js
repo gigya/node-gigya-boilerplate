cc576331e191ee577c9f82da7ee6999678af6ed8
Max Mckenzie <hello@maxmckenzie.uk>
2017-03-27 17:39:02 +0200

    update to circle ci and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+      "standard",
+      "promise"
+    ],
+    "globals"   : {
+      /* testing globals */
+      "describe"   : false,
+      "it"         : false,
+      "before"     : false,
+      "beforeEach" : false,
+      "after"      : false,
+      "afterEach"  : false
+  }
+};
