256b214695611e81c4b47372971c94c8557daca4
Max Mckenzie <hello@maxmckenzie.uk>
2017-03-28 16:26:14 +0200

    removed module exports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import http from 'http'
 
-const app = http.createServer((req, res) => {
+http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
 }).listen(3000, '127.0.0.1')
 
-process.title = app
-
 console.log('Server running at http://127.0.0.1:3000/')
-
-module.exports = app
