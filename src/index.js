0a250e1fc3a2b488bc5f236e7ea106512cb5ee34
Max Mckenzie <hello@maxmckenzie.uk>
2017-03-28 16:19:49 +0200

    killall after npm test to stop server hanging, could be better it may be advisable to pull this logic into the server and hook it on the before and after

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import http from 'http'
 
-http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
 }).listen(3000, '127.0.0.1')
 
+process.title = app
+
 console.log('Server running at http://127.0.0.1:3000/')
+
+module.exports = app
